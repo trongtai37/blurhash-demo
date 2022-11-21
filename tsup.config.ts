033f1b88a2079451caa861c30f3cc1361c88cb3e
Tai Nguyen Trong <trongtaind37@gmail.com>
2022-11-21 16:26:54 +0700

    initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+  {
+    name: "main",
+    entry: ["./src/index.ts"],
+    outDir: "./dist",
+    format: ["cjs", "esm"],
+    legacyOutput: true,
+    sourcemap: true,
+    clean: true,
+    splitting: false,
+    dts: false,
+    minify: true,
+  },
+  {
+    name: "esm",
+    entry: ["./src/index.ts"],
+    outDir: "./dist",
+    format: ["esm"],
+    legacyOutput: false,
+    sourcemap: true,
+    clean: true,
+    splitting: false,
+    dts: false,
+    minify: true,
+  },
+  {
+    name: "typedefs",
+    entry: ["./src/index.ts"],
+    outDir: "./dist",
+    clean: false,
+    dts: {
+      only: true,
+    },
+  },
+]);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/demo.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "demo.js",
+    path: path.resolve(__dirname, "demo"),
+  },
+  devServer: {
+    static: path.join(__dirname, "demo"),
+    compress: true,
+    port: 9000,
+  },
+};
